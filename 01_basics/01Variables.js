654abdc424666083b2d20400b1f955dc51d930c4
SHASHI YADAV <shashi.yadav.ug23@nsut.ac.in>
2025-05-13 03:31:44 +0000

    variablesGyan

---
+++ 01_basics/01Variables.js
@@ -0,0 +1,13 @@
+const accountId=123343
+let accountEmail="shashi@gmail.com"
+var accountPassword="12345"
+accountCity="delhi"
+
+//changing 
+
+// accountId=22
+// console.log(accountId);
+accountEmail="xyz@gmail.com";
+accountPassword="32435";
+accountCity="jaipur";
+console.table([accountEmail,accountPassword,accountCity]);
