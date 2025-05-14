0fdca69a03e9ad4571340d5529803a8345ccc947
SHASHI YADAV <shashi.yadav.ug23@nsut.ac.in>
2025-05-14 05:43:17 +0000

    string

---
+++ 01_basics/stringNumber.js
@@ -0,0 +1,25 @@
+console.log("hi ..we are learning operations")
+//operations
+
+
+let value=3
+let negval= -value
+console.log (negval)
+
+let str1 = "hello"
+let str2 = " shashi"
+let str3 = str1+str2
+console.log(str3)
+
+//but but
+console.log("1"+2) //12
+console.log(1+"2") //12
+console.log("1"+2+2) //122
+console.log(1+2+"2") //32
+
+
+//----------------------------------------------------------------------------------------
+//lec 8 : COMPARISON
+console.log(null>0)
+console.log(null>=0)
+console.log(null==0)
